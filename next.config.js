ece07c34285bc95b93649074c457d175c56259e6
solufa <solufa2020@gmail.com>
2022-08-26 02:25:11 +0900

    chore: update nextjs

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
 }
