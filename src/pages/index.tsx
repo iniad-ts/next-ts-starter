36f1731c55aa6a8af1d0205f727b7ac3693f6f72
solufa <solufa2020@gmail.com>
2023-04-26 06:09:57 +0900

    refactor: use css modules

---
+++ src/pages/index.tsx
@@ -0,0 +1,69 @@
+import Button from '@mui/material/Button'
+import styles from './index.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
+            pages/index.js
+          </code>
+        </p>
+
+        <div className={styles.grid}>
+          <a className={styles.card} href="https://nextjs.org/docs">
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a className={styles.card} href="https://nextjs.org/learn">
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            className={styles.card}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          </a>
+        </div>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          href="https://github.com/mui/material-ui/tree/v5.10.8/docs/data/material/getting-started/templates/dashboard"
+        >
+          material-ui
+        </Button>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
--- stylelint.config.js
+++ stylelint.config.js
@@ -5,12 +5,15 @@ module.exports = {
     'stylelint-config-styled-components',
     'stylelint-config-prettier',
   ],
+  plugins: ['stylelint-order'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
     'value-keyword-case': null,
     'unit-no-unknown': null,
     'function-no-unknown': null,
+    'order/order': ['custom-properties', 'declarations'],
+    'order/properties-order': [],
   },
   customSyntax: '@stylelint/postcss-css-in-js',
 }
