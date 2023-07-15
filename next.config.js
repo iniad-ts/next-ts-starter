30e425cdf45e3b398b16277d192366573bcf7845
solufa <solufa2020@gmail.com>
2023-07-15 16:17:48 +0900

    refactor: set strict-boolean-expressions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
+  basePath:
+    process.env.GITHUB_REPOSITORY !== undefined
+      ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+      : '',
   output: 'export',
   trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
