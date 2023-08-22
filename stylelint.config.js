a7fb27a014e6955026fbf87b865ae64acba898cb
solufa <solufa2020@gmail.com>
2023-08-23 04:52:56 +0900

    chore: update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
+  },
 };
