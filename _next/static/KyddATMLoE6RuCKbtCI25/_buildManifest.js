b025476270cda513524fc6f4959c8dab8fdf9d66
AllenShintani <AllenShintani@users.noreply.github.com>
2024-04-09 05:38:59 +0000

    deploy: 2ee92bb477df333382bdb4fa001bfd9a6a9a64ab

---
+++ _next/static/KyddATMLoE6RuCKbtCI25/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a3d1503c392946df.css","static/chunks/pages/index-322bf91d415321de.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Nxnr1Oli_pY20GUnjHqJU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a3d1503c392946df.css","static/chunks/pages/index-66d8d531c5ed7997.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
