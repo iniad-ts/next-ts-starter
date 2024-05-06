5aaadd8b35115bf51ddac4c1ecea6dada121b514
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-05-07 01:22:07 +0900

    chore: fix plugin name in stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recess-order',
-    'stylelint-config-prettier',
+    'stylelint-prettier/recommended',
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
